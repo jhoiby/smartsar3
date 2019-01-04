b3d8a53c2ad4a0d842efbe4e7144f6b62f2b6e52
James Hoiby <james@hoiby.com>
2019-01-03 22:25:47 -0700

    AddNotification method.

---
+++ Contexts.Common.UnitTests/Notifications/NotificationListTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shouldly;
+using SSar.Contexts.Common.Notifications;
+using Xunit;
+
+namespace SSar.Contexts.Common.UnitTests.Notifications
+{
+    public class NotificationListTests
+    {
+        [Fact]
+        public void AddNotification_adds_complete_notification()
+        {
+            var notificationList = new NotificationList();
+
+            notificationList.AddNotification("message1", "key1");
+
+            notificationList.ShouldSatisfyAllConditions(
+                () => notificationList.First().Message.ShouldBe("message1"),
+                () => notificationList.First().ForField.ShouldBe("key1"));
+        }
+
+        [Fact]
+        public void AddNotification_returns_NotificationList()
+        {
+            var notificationList = new NotificationList();
+
+            var list2 = notificationList.AddNotification("message1", "key1");
+        }
+    }
+}
--- Contexts.Common/Notifications/NotificationList.cs
+++ Contexts.Common/Notifications/NotificationList.cs
@@ -6,5 +6,11 @@ namespace SSar.Contexts.Common.Notifications
 {
     public class NotificationList : List<Notification>
     {
+        public NotificationList AddNotification(string message, string fieldKey= "")
+        {
+            this.Add(new Notification(message, fieldKey));
+
+            return this;
+        }
     }
 }
