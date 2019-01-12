9def073635f4ad2005ec552085f6b8bfbf9c085e
James Hoiby <james@hoiby.com>
2019-01-11 18:36:05 -0700

    Extend registration UI for new user fields.

---
+++ Presentation.WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SSar.Presentation.WebUI.Areas.Identity.Models;
+using SSar.Presentation.WebUI.Data;
+
+[assembly: HostingStartup(typeof(SSar.Presentation.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace SSar.Presentation.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
