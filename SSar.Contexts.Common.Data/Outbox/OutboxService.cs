b9a51d1d2346ded58227ddb88bde0f6c07571223
James Hoiby <james@hoiby.com>
2019-01-24 23:34:26 -0700

    Add exception for null outbox provider.

--- SSar.Contexts.Common.Data/Outbox/OutboxService.cs
+++ SSar.Contexts.Common.Data/Outbox/OutboxService.cs
@@ -22,26 +22,40 @@ namespace SSar.Contexts.Common.Data.Outbox
         public bool ProviderIsSet => _dbContext != null;
 
         public void Delete(Guid id)
-        {
+        {   
+            ThrowIfNoProvider();
             _dbContext.Remove(_dbContext.OutboxPackages.Single(p => p.PackageId == id));
         }
 
         public void DeleteRange(Guid[] idArray)
         {
+            ThrowIfNoProvider();
             _dbContext.RemoveRange(
                 _dbContext.OutboxPackages.Where(p => idArray.Contains(p.PackageId)));
         }
 
         public void AddObject(Guid objectId, string objectType, object @object, DateTime validUntil)
         {
-            var serializedObject = JsonConvert.SerializeObject(@object);
-
             // TODO: Try/catch with custom Outbox exception
+
+            ThrowIfNoProvider();
+
+            var serializedObject = JsonConvert.SerializeObject(@object);
 
             _dbContext.Add<OutboxPackage>(
                 new OutboxPackage(objectId, objectType, serializedObject, validUntil));
 
-            // Will be written to DB later when handler calls AppDbContext.SaveAsync
+            // Will be persisted to DB later when handler calls AppDbContext.SaveAsync
+        }
+
+        private void ThrowIfNoProvider()
+        {
+            if (!ProviderIsSet)
+            {
+                throw new InvalidOperationException(
+                    "A database context provider must be passed to AddProvider()" +
+                    " before using the OutboxService.");
+            }
         }
     }
 }
