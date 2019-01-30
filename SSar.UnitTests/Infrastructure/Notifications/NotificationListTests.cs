09bfb1a74730179ad64893480a9a6a20b81cb07a
James Hoiby <james@hoiby.com>
2019-01-30 11:41:25 -0700

    Add additional tests.

--- SSar.UnitTests/Infrastructure/Notifications/NotificationListTests.cs
+++ SSar.UnitTests/Infrastructure/Notifications/NotificationListTests.cs
@@ -104,6 +104,7 @@ namespace SSar.UnitTests.Infrastructure.Notifications
                 .AddNotification("param3", "message1 for param3");
 
             var targetNotificationList = new NotificationList()
+                .AddNotification("param1", "message1 for param1") // Intentional duplicate
                 .AddNotification("param1", "message3 for param1")
                 .AddNotification("param1", "message4 for param1")
                 .AddNotification("param2", "message2 for param2")
@@ -112,12 +113,18 @@ namespace SSar.UnitTests.Infrastructure.Notifications
             targetNotificationList.AddOrAppend(sourceNotificationList);
 
             targetNotificationList.ShouldSatisfyAllConditions(
-                () => targetNotificationList["param1"].Count.ShouldBe(4),
+                () => targetNotificationList["param1"].Count.ShouldBe(5),
                 () => targetNotificationList["param2"].Count.ShouldBe(2),
                 () => targetNotificationList["param3"].Count.ShouldBe(1),
                 () => targetNotificationList["param4"].Count.ShouldBe(1),
-                () => targetNotificationList["param1"].Any(n => n.Message == "message2 for param1").ShouldBeTrue(),
-                () => targetNotificationList["param1"].Any(n => n.Message == "message4 for param1").ShouldBeTrue());
+                () => targetNotificationList["param1"].ShouldContain(n => n.Message.Contains("message1 for param1"), 2),
+                () => targetNotificationList["param1"].ShouldContain(n => n.Message.Contains("message2 for param1"), 1),
+                () => targetNotificationList["param1"].ShouldContain(n => n.Message.Contains("message3 for param1"), 1),
+                () => targetNotificationList["param1"].ShouldContain(n => n.Message.Contains("message4 for param1"), 1),
+                () => targetNotificationList["param2"].ShouldContain(n => n.Message.Contains("message1 for param2"), 1),
+                () => targetNotificationList["param2"].ShouldContain(n => n.Message.Contains("message2 for param2"), 1),
+                () => targetNotificationList["param3"].ShouldContain(n => n.Message.Contains("message1 for param3"), 1),
+                () => targetNotificationList["param4"].ShouldContain(n => n.Message.Contains("message1 for param4"), 1));
         }
     }
 }
