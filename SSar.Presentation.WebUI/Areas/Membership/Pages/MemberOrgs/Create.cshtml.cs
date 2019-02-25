4c7872fc924b1738720efe325578bca0ea8ea180
James Hoiby <james@hoiby.com>
2019-02-24 21:20:33 -0700

    Add member org create page.

---
+++ SSar.Presentation.WebUI/Areas/Membership/Pages/MemberOrgs/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SSar.Contexts.Membership.Application.Commands;
+using SSar.Presentation.WebUI.SharedModels;
+
+namespace SSar.Presentation.WebUI.Areas.Membership.Pages.MemberOrgs
+{
+    public class CreateModel : CommandPageModel
+    {
+        [BindProperty]
+        public CreateMembershipOrganizationCommand Command { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return
+                ReturnNotificationsOrRedirectToPage(
+                    await Mediator.Send(Command),
+                    "Index");
+        }
+    }
+}
