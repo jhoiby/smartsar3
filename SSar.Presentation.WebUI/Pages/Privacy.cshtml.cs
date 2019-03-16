06861b0923a7fce4ba3970de8578b712bf8f5d5d
James Hoiby <james@hoiby.com>
2019-03-16 10:16:15 -0700

    Allow anonymous access on selected pages.

--- SSar.Presentation.WebUI/Pages/Privacy.cshtml.cs
+++ SSar.Presentation.WebUI/Pages/Privacy.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SSar.Presentation.WebUI.Pages
 {
+    [AllowAnonymous]
     public class PrivacyModel : PageModel
     {
         public void OnGet()
